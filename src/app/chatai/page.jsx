642a2194e4d60fb5d7449c7f6c0d496375a67bbe
Kartikey <kartikeypathak08@gmail.com>
2026-03-23 23:46:10 +0530

    added typing effect of ai reply

--- src/app/chatai/page.jsx
+++ src/app/chatai/page.jsx
@@ -145,13 +145,33 @@ export default function Chat() {
             updatedChatList[chatIndex].title = resp.data.title;
 
             // Add The LLm Reply In State 
+            // 1. add empty message first
             updatedChatList[chatIndex].messages.push({
                 role: "assistant",
-                content: resp.data.message
+                content: ""
             });
-
-
             setChatList([...updatedChatList]);
+            // 2. typing effect
+            const fullText = resp.data.message;
+            let i = 0;
+            const interval = setInterval(() => {
+                const speed = 5;
+
+                updatedChatList[chatIndex].messages[
+                    updatedChatList[chatIndex].messages.length - 1
+                ].content += fullText.slice(i, i + speed);
+
+                setChatList([...updatedChatList]);
+
+                i += speed;
+
+                if (i >= fullText.length) {
+                    clearInterval(interval);  //Stop this loop
+                    setLoading(false);
+                }
+            }, 15); // speed (lower = faster)
+
+
         } catch (error) {
             console.log(error);
         }
@@ -200,133 +220,133 @@ export default function Chat() {
 
     return (
         <>
-        <section className=" min-h-screen relative h-full pt-12 overflow-hidden w-full flex items-center ">
-            <nav className=" h-12 bg-black w-screen  fixed top-0 z-50 border-b-2 border-[#CBCBCB] flex-row flex justify-between items-center p-5 dark:border-[#303030]">
-                <Link href="/" className="dark:text-white text-white text-2xl font-medium">LLama  </Link>
+            <section className=" min-h-screen relative h-full pt-12 overflow-hidden w-full flex items-center ">
+                <nav className=" h-12 bg-black w-screen  fixed top-0 z-50 border-b-2 border-[#CBCBCB] flex-row flex justify-between items-center p-5 dark:border-[#303030]">
+                    <Link href="/" className="dark:text-white text-white text-2xl font-medium">LLama  </Link>
 
-                {side ?
-                    <div>
-                        <div className="drawer">
-                            <input id="my-drawer-1" type="checkbox" className="drawer-toggle" />
-                            <div className="drawer-content ">
-                                {/* Page content here */}
-                                <label htmlFor="my-drawer-1" className=" drawer-button"><i class="fa-solid fa-bars text-2xl cursor-pointer hover:text-gray-500 text-white transition-all active:text-gray-500 active:dark:text-gray-500"></i></label>
-                            </div>
-                            <div className="drawer-side ">
-                                <label htmlFor="my-drawer-1" aria-label="close sidebar" className="drawer-overlay bg-white/30 dark:bg-black/30 transition-all backdrop-blur-sm"></label>
+                    {side ?
+                        <div>
+                            <div className="drawer">
+                                <input id="my-drawer-1" type="checkbox" className="drawer-toggle" />
+                                <div className="drawer-content ">
+                                    {/* Page content here */}
+                                    <label htmlFor="my-drawer-1" className=" drawer-button"><i class="fa-solid fa-bars text-2xl cursor-pointer hover:text-gray-500 text-white transition-all active:text-gray-500 active:dark:text-gray-500"></i></label>
+                                </div>
+                                <div className="drawer-side ">
+                                    <label htmlFor="my-drawer-1" aria-label="close sidebar" className="drawer-overlay bg-white/30 dark:bg-black/30 transition-all backdrop-blur-sm"></label>
 
-                                <div className=" h-full w-80 bg-gray-200 dark:bg-[#181818] overflow-hidden">
-                                    <div className="  h-12 mt-20 justify-between px-15 flex items-center flex-row gap-5">
-                                        <h2 className=" m-1 font-semibold text-black dark:text-[#AFAFAF] ">Your Chats</h2>
-                                        <button
-                                            onClick={() => {
-                                                setActiveChatIndex(null);
-                                                toast.success("New Chat Created..");
-                                            }
-                                            }
-                                            className="text-2xl size-9 flex items-center justify-center bg-[#242424] hover:dark:bg-[#242424]/50 active:bg-[#242424]/50 hover:bg-[#242424]/50 rounded-full cursor-pointer text-white hover:text-gray-300"
-                                        >
-                                            <i className=" text-white text-sm font-semibold fa-solid fa-plus"></i>
-                                        </button>
-                                    </div>
-                                    <div className="  mt-10 w-full h-full flex-col ml-5  gap-3 flex justify-start items-start overflow-y-scroll">
+                                    <div className=" h-full w-80 bg-gray-200 dark:bg-[#181818] overflow-hidden">
+                                        <div className="  h-12 mt-20 justify-between px-15 flex items-center flex-row gap-5">
+                                            <h2 className=" m-1 font-semibold text-black dark:text-[#AFAFAF] ">Your Chats</h2>
+                                            <button
+                                                onClick={() => {
+                                                    setActiveChatIndex(null);
+                                                    toast.success("New Chat Created..");
+                                                }
+                                                }
+                                                className="text-2xl size-9 flex items-center justify-center bg-[#242424] hover:dark:bg-[#242424]/50 active:bg-[#242424]/50 hover:bg-[#242424]/50 rounded-full cursor-pointer text-white hover:text-gray-300"
+                                            >
+                                                <i className=" text-white text-sm font-semibold fa-solid fa-plus"></i>
+                                            </button>
+                                        </div>
+                                        <div className="  mt-10 w-full h-full flex-col ml-5  gap-3 flex justify-start items-start overflow-y-scroll">
 
-                                        {chatList.map((chat, index) => (
-                                            <div onClick={() => setActiveChatIndex(index)} key={index} className={` bg-[#242424] ${activeChatIndex === index ? "dark:bg-[#242424] bg-[#242424]/50 " : "bg-[#242424]/20"} justify-between flex-row h-10 w-56 flex items-center px-6 rounded-4xl`}>
-                                                <h2>{chat.title}</h2>
-                                                <i onClick={() => { deletechat(index) }} className=" text-red-500 font-semibold cursor-pointer hover:dark:text-red-300 hover:text-red-300 transition-all active:text-red-300 active:dark:text-red-300 fa-solid fa-trash"></i>
-                                            </div>
-                                        ))}
+                                            {chatList.map((chat, index) => (
+                                                <div onClick={() => setActiveChatIndex(index)} key={index} className={` bg-[#242424] ${activeChatIndex === index ? "dark:bg-[#242424] bg-[#242424]/50 " : "bg-[#242424]/20"} justify-between flex-row h-10 w-56 flex items-center px-6 rounded-4xl`}>
+                                                    <h2>{chat.title}</h2>
+                                                    <i onClick={() => { deletechat(index) }} className=" text-red-500 font-semibold cursor-pointer hover:dark:text-red-300 hover:text-red-300 transition-all active:text-red-300 active:dark:text-red-300 fa-solid fa-trash"></i>
+                                                </div>
+                                            ))}
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    </div> : null
-                }
+                        </div> : null
+                    }
 
-            </nav>
-            
-            <section className=" flex-row  flex dark:bg-[#212121]  xl:h-[93vh] h-[92vh] w-full bg-white ">
-                <Toaster />
-                {
-                    loadingchat ?
-                        <span className=" fixed z-50 left-0 right-0 mx-auto bottom-0 top-0 my-auto loading loading-spinner loading-xl text-white"></span> : null
-                }
-                {/* side Area */}
-                {!side ?
-                    <div className=" h-full w-80 bg-[#F9FAFB] dark:bg-[#181818] overflow-hidden">
-                        <div className="  h-12 mt-20 justify-between px-5 flex items-center flex-row gap-5">
-                            <h2 className=" m-1 font-semibold text-[#AFAFAF] ">Your Chats</h2>
-                            <button
-                                onClick={() => {
-                                    setActiveChatIndex(null);
-                                    toast.success("New Chat Created..");
-                                }}
-                                className="text-2xl size-9 flex items-center justify-center bg-[#242424] hover:dark:bg-[#242424]/50 active:bg-[#242424]/50 hover:bg-[#242424]/50 rounded-full cursor-pointer text-white hover:text-gray-300"
-                            >
-                                <i className=" text-white text-sm font-semibold fa-solid fa-plus"></i>
-                            </button>
-                        </div>
-                        <div className="  mt-10 w-full h-full flex-col  gap-3 flex justify-start items-center overflow-y-scroll">
+                </nav>
 
-                            {chatList.map((chat, index) => (
-                                <div onClick={() => setActiveChatIndex(index)} key={index} className={` bg-[#242424] ${activeChatIndex === index ? "bg-[#242424]" : "bg-[#242424]/20"} justify-between flex-row h-10 w-56 flex items-center px-6 rounded-4xl`}>
-                                    <h2 className=" dark:text-white text-white">{chat.title}</h2>
-                                    <i onClick={() => { deletechat(index) }} className=" text-red-500 font-semibold cursor-pointer hover:dark:text-red-300 hover:text-red-300 transition-all active:text-red-300 active:dark:text-red-300 fa-solid fa-trash"></i>
-                                </div>
-                            ))}
-                        </div>
-                    </div> : null
-                }
-
-                {/* chat area */}
-
-                <div className=" px-1  lg:px-5 xl:px-7 w-full h-full flex flex-col items-center justify-center">
-
-                    <div className="   w-full h-screen mb-15  overflow-y-scroll ">
-
-                        {activeChatIndex !== null &&
-                            chatList[activeChatIndex]?.messages?.map((msg, index) => (
-                                <div
-                                    key={index}
-                                    className={`chat ${msg.role === "user" ? "chat-end" : "chat-start"}`}
+                <section className=" flex-row  flex dark:bg-[#212121]  xl:h-[93vh] h-[92vh] w-full bg-white ">
+                    <Toaster />
+                    {
+                        loadingchat ?
+                            <span className=" fixed z-50 left-0 right-0 mx-auto bottom-0 top-0 my-auto loading loading-spinner loading-xl text-white"></span> : null
+                    }
+                    {/* side Area */}
+                    {!side ?
+                        <div className=" h-full w-80 bg-[#F9FAFB] dark:bg-[#181818] overflow-hidden">
+                            <div className="  h-12 mt-20 justify-between px-5 flex items-center flex-row gap-5">
+                                <h2 className=" m-1 font-semibold text-[#AFAFAF] ">Your Chats</h2>
+                                <button
+                                    onClick={() => {
+                                        setActiveChatIndex(null);
+                                        toast.success("New Chat Created..");
+                                    }}
+                                    className="text-2xl size-9 flex items-center justify-center bg-[#242424] hover:dark:bg-[#242424]/50 active:bg-[#242424]/50 hover:bg-[#242424]/50 rounded-full cursor-pointer text-white hover:text-gray-300"
                                 >
+                                    <i className=" text-white text-sm font-semibold fa-solid fa-plus"></i>
+                                </button>
+                            </div>
+                            <div className="  mt-10 w-full h-full flex-col  gap-3 flex justify-start items-center overflow-y-scroll">
+
+                                {chatList.map((chat, index) => (
+                                    <div onClick={() => setActiveChatIndex(index)} key={index} className={` bg-[#242424] ${activeChatIndex === index ? "bg-[#242424]" : "bg-[#242424]/20"} justify-between flex-row h-10 w-56 flex items-center px-6 rounded-4xl`}>
+                                        <h2 className=" dark:text-white text-white">{chat.title}</h2>
+                                        <i onClick={() => { deletechat(index) }} className=" text-red-500 font-semibold cursor-pointer hover:dark:text-red-300 hover:text-red-300 transition-all active:text-red-300 active:dark:text-red-300 fa-solid fa-trash"></i>
+                                    </div>
+                                ))}
+                            </div>
+                        </div> : null
+                    }
+
+                    {/* chat area */}
+
+                    <div className=" px-1  lg:px-5 xl:px-7 w-full h-full flex flex-col items-center justify-center">
+
+                        <div className="   w-full h-screen mb-15  overflow-y-scroll ">
+
+                            {activeChatIndex !== null &&
+                                chatList[activeChatIndex]?.messages?.map((msg, index) => (
                                     <div
-                                        className={` rounded-xl px-4 chat-content py-3 chat-bubble ${msg.role === "user" ? "dark:bg-[#303030] bg-gray-200 text-black dark:text-white " : " bg-transparent text-gray-800 dark:text-white chat-start"}`}
-                                        dangerouslySetInnerHTML={{ __html: msg.content }}
-                                    />
+                                        key={index}
+                                        className={`chat ${msg.role === "user" ? "chat-end" : "chat-start"}`}
+                                    >
+                                        <div
+                                            className={` rounded-xl px-4 chat-content py-3 chat-bubble ${msg.role === "user" ? "dark:bg-[#303030] bg-gray-200 text-black dark:text-white " : " bg-transparent text-gray-800 dark:text-white chat-start"}`}
+                                            dangerouslySetInnerHTML={{ __html: msg.content }}
+                                        />
+                                    </div>
+                                ))}
+                            {/* Loader */}
+                            {loading && activeChatIndex !== null && (
+                                <div className="chat chat-start">
+                                    <div className="rounded-xl px-4 py-3 chat-bubble bg-transparent text-black dark:text-white">
+                                        <span className="loading loading-ball loading-xl"></span>
+                                    </div>
                                 </div>
-                            ))}
-                        {/* Loader */}
-                        {loading && activeChatIndex !== null && (
-                            <div className="chat chat-start">
-                                <div className="rounded-xl px-4 py-3 chat-bubble bg-transparent text-black dark:text-white">
-                                    <span className="loading loading-ball loading-xl"></span>
+                            )}
+
+                            {activeChatIndex === null &&
+                                <div className=" w-full h-full flex items-center justify-center">
+                                    <TextChange />
                                 </div>
-                            </div>
-                        )}
-
-                        {activeChatIndex === null &&
-                            <div className=" w-full h-full flex items-center justify-center">
-                                <TextChange />
-                            </div>
-                        }
+                            }
 
 
 
-                        <div ref={chatRef} />
+                            <div ref={chatRef} />
+                        </div>
+
+                        <div className=" fixed   md:mb-1 gap-2  bottom-5  w-full flex items-center justify-center">
+                            <PlaceholdersAndVanishInput
+                                placeholders={placeholders}
+                                onChange={handleChange}
+                                onSubmit={handleSubmit}
+                            />
+                        </div>
                     </div>
 
-                    <div className=" fixed   md:mb-1 gap-2  bottom-5  w-full flex items-center justify-center">
-                        <PlaceholdersAndVanishInput
-                            placeholders={placeholders}
-                            onChange={handleChange}
-                            onSubmit={handleSubmit}
-                        />
-                    </div>
-                </div>
-
-            </section>
+                </section>
             </section>
         </>
     );
